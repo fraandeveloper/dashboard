830855ac979fc1b0235c3ca0ace762fdd4ed1985
Francisco <franciscobarros754@gmail.com>
2019-01-18 06:12:24 -0200

    adicionando dashboad central

--- src/app/sistema/panel/dashboard/dashboard.component.ts
+++ src/app/sistema/panel/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotoCuratedService } from '../../core/photo/photo-curated.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+ 
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit() {
   }
