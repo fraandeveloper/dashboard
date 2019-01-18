bb40950d762b00010a5e1e7566ce9fa9f84a7365
Francisco <franciscobarros754@gmail.com>
2019-01-18 06:10:07 -0200

    adicionando url api base em enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiURL: 'https://us-central1-trakto-dev.cloudfunctions.net/unsplashApi/api/v1/photos/'
 };
 
 /*
